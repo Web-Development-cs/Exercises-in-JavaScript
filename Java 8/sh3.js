04d23f9e73616582356b9722c19dc48f781095b5
Blerina Demaj <95138459+blerinademaj@users.noreply.github.com>
2022-10-22 22:48:50 +0200

    Pjesa 2

---
+++ Java 8/sh3.js
@@ -0,0 +1,10 @@
+
+function funksioni(){
+
+    a = 80;
+    piketEStudentit = (a<50)? "Studenti nuk kalon":
+                              "Studenti kalon";
+    document.write(piketEStudentit);
+}
+
+funksioni();
